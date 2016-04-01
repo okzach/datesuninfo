fb2b30b643f2bc3fd066d46cfdc31cf91f07d8d5
Zach Adams <zach@okzach.com>
2016-04-01 08:04:14 -0500

    added basic classifiers

--- setup.py
+++ setup.py
@@ -13,5 +13,14 @@ setup(
         'console_scripts': [
             'datesuninfo=datesuninfo:main'
         ]
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ]
 )
