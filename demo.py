e1a8f4019f99a6c74df96dbb65400f42c1f1807d
Zach Adams <zach@okzach.com>
2017-05-19 14:52:47 -0500

    Updated demo.py for python 3 compatibility.

--- demo.py
+++ demo.py
@@ -1,5 +1,9 @@
+from __future__ import print_function
+
 import datetime
+
 import pytz
+
 from datesuninfo import date_sun_info
 
 
@@ -11,8 +15,8 @@ calc_date = calc_date.astimezone(pytz.timezone('US/Central'))
 # Get the date_sun_info for Moore, OK using the current time in the central time zone.
 sun_info = date_sun_info(latitude='35.3484055', longitude='-97.48163', calc_date=calc_date)
 
-print 'Sunrise: {0}'.format(sun_info.get('sunrise'))
-print 'Solar Noon: {0}'.format(sun_info.get('transit'))
-print 'Sunset: {0}'.format(sun_info.get('sunset'))
+print('Sunrise: {0}'.format(sun_info.get('sunrise')))
+print('Solar Noon: {0}'.format(sun_info.get('transit')))
+print('Sunset: {0}'.format(sun_info.get('sunset')))
 
-print sun_info.keys()
+print(sun_info.keys())
